43324c741bb33661a60ced5dedea5bf48696a33a
liujeff9 <liujeff@seas.upenn.edu>
2021-04-26 19:50:19 -0400

    simple webcam

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response, jsonify
 from camera import VideoCamera
+import cv2
 
 app = Flask(__name__)
 
@@ -17,4 +18,8 @@ def gen(camera):
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(video_stream), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(video_stream),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True,port="5000")
--- src/webapp/camera.py
+++ src/webapp/camera.py
@@ -10,6 +10,8 @@ class VideoCamera(object):
     def get_frame(self):
         ret, frame = self.video.read()
 
+        # DO WHAT YOU WANT WITH TENSORFLOW / KERAS AND OPENCV
+
         ret, jpeg = cv2.imencode('.jpg', frame)
 
         return jpeg.tobytes()
